ec24e3b1612610dc9f77a874d130e1a7ddcf6140
swaggy <sakshamg.782@gmail.com>
2023-05-07 16:58:37 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dummyChats = require("./DummyData/dummyData");
+
+const app = express();
+
+// To check if the server is running...
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
+
+app.get("/api/v1/chat", (req, res) => {
+  res.send(dummyChats);
+});
+
+// console.log(dummyChats);
+
+app.listen(5001, console.log("Server is running on PORT: 5001"));
